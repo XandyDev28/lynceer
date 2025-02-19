1df0259083ed618250e602d53cfea8c5a35ef0d7
XandyDev28 <alexaaandreeecaastrooo@gmail.com>
2025-02-19 19:40:13 -0300

    rename filesTESTE

---
+++ lynceer/src/main/java/br/com/lynceer/lynceer/LynceerApplication.java
@@ -0,0 +1,13 @@
+package br.com.lynceer.lynceer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LynceerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LynceerApplication.class, args);
+	}
+
+}
---
+++ lynceer/src/test/java/br/com/lynceer/lynceer/LynceerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lynceer.lynceer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LynceerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
