6a30054c9f3ff024bc2ed1f7b6942eed10d1b10a
XandyDev28 <alexaaandreeecaastrooo@gmail.com>
2025-02-19 20:31:18 -0300

    mensagens de erro no login2, em andamento...

--- lynceer/src/main/resources/templates/js/login.js
+++ lynceer/src/main/resources/templates/js/login.js
@@ -1,7 +1,15 @@
 function btnLogin(){
-    if (document.getElementById("txtemail").value == ""){
-        alert("COLOQUE SEU EMAIL!")
+
+var email = document.getElementById("txtemail")
+var resultadoemail = document.getElementById("resultadoemail")
+var resultadosenha = document.getElementById("resultadosenha")
+
+    if (email.value == ""){
+        resultadoemail.innerHTML = "DIgite seu email"
+        resultadosenha.innerHTML = "DIgite sua senha"
     } else {
         alert("LOGOU")
+         resultadoemail.innerHTML = ""
+        resultadosenha.innerHTML = ""
     }
 }
